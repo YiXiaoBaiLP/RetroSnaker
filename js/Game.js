ad1af2e5b224d3ba881f2e6cd1e8cec7334424bf
YiXiaoBai <yixiaobai.lp@qq.com>
2023-08-03 09:44:15 +0800

    添加相关注释

--- js/Game.js
+++ js/Game.js
@@ -3,8 +3,10 @@
 // function Game(){
 //     alert("执行了！");
 // }
-// 使用IIFE进行隔离（使用IIFE隔离作用域）
+// 使用IIFE包裹起来进行隔离（使用IIFE隔离作用域），避免打包时出现冲突
+// (function() {})() 可以将这个方法变成 window 的一个属性
 (function () {
+    // 赋值给 window 中的一个属性；主要用于隔绝作用域
     window.Game = function () {
         // 初始化
         // 设置表格的大小
@@ -13,6 +15,7 @@
         // 初始化
         this.init();
     }
+    // 初始化游戏中的表格
     Game.prototype.init = function () {
         var $table = $("<table></table>");
         // 渲染表格
