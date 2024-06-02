efcda2573d2c09482a9c3afe27bbc7ca4149f289
YiXiaoBai <yixiaobai.lp@qq.com>
2024-06-02 18:55:42 +0800

    给游戏添加 向左向右移动的功能

--- js/Block.js
+++ js/Block.js
@@ -1,5 +1,5 @@
-(function () {
-  window.Block = function () {
+(function() {
+  window.Block = function() {
     // 得到随机的方块
     // 第一步：罗列所有的类型
     var allType = ["S", "T", "O", "L", "J", "I", "Z"];
@@ -16,7 +16,7 @@
     //  初始的列，因为要居中显示，所以列要为4
     this.col = 6;
   };
-  Block.prototype.render = function () {
+  Block.prototype.render = function() {
     // 渲染四行四列的方块
     for (var i = 0; i < 4; i++) {
       for (var j = 0; j < 4; j++) {
@@ -29,7 +29,7 @@
       }
     }
   };
-  Block.prototype.check = function (row, col) {
+  Block.prototype.check = function(row, col) {
     // check 函数的row 和col 指的是要校验的地图的 row 和 col 的位置
     // 能力判断方法，判断的是对应位置的方块和地图位置的方块是否有都不为0的情况，如果有返回 true，否则返回false：就代表没有重合
     for (var i = 0; i < 4; i++) {
@@ -42,7 +42,7 @@
     return true;
   };
   // 方块下落，需要判断当前的方块是否能够下落
-  Block.prototype.checkDown = function () {
+  Block.prototype.checkDown = function() {
     // 判断当前地图的位置和自己方块的位置是否有重合this.row + 1 指的是预判
     // 预判断就是在下一次方块将要到达的位置是否有对应的地图方块不为0
     if (this.check(this.row + 1, this.col)) {
@@ -50,6 +50,35 @@
     } else {
       // 此时就是下落到底的状态，渲染新的方块
       game.block = new Block();
+      // 方块到底了，然后要渲染到地图的code 中
+      this.renderMap();
     }
   };
+  // 判断是否能够向左移动，如果可以则移动
+  Block.prototype.checkLeft = function() {
+    // 判断是否可以向左
+    if (this.check(this.row, this.col - 1)) {
+      this.col--;
+    }
+  };
+  // 判断是否能够向右移动，如果可以则移动
+  Block.prototype.checkRight = function() {
+    // 判断是否可以向左
+    if (this.check(this.row, this.col + 1)) {
+      this.col++;
+    }
+  };
+  // 将已经到底的方块渲染到地图中
+  Block.prototype.renderMap = function() {
+    for (var i = 0; i < 4; i++) {
+      for (var j = 0; j < 4; j++) {
+        // 将现在已有的方块渲染到Map类的mapCode上
+        if (this.code[i][j] !== 0) {
+          // 改变爹图的mapCode数据
+          game.map.mapCode[this.row + i][this.col + j] = this.code[i][j];
+        }
+      }
+    }
+  }
+
 })();
--- js/Game.js
+++ js/Game.js
@@ -5,9 +5,9 @@
 // }
 // 使用IIFE包裹起来进行隔离（使用IIFE隔离作用域），避免打包时出现冲突
 // (function() {})() 可以将这个方法变成 window 的一个属性
-(function () {
+(function() {
   // 赋值给 window 中的一个属性；主要用于隔绝作用域
-  window.Game = function () {
+  window.Game = function() {
     // 初始化
     // 设置表格的大小
     this.row = 20;
@@ -20,9 +20,10 @@
     this.map = new Map();
     // 启动定时器
     this.start();
+    this.bindEvent();
   };
   // 初始化游戏中的表格
-  Game.prototype.init = function () {
+  Game.prototype.init = function() {
     var $table = $("<table></table>");
     // 渲染表格
     for (var i = 0; i < this.row; i++) {
@@ -37,7 +38,7 @@
     }
     $($table).appendTo("body");
   };
-  Game.prototype.setColor = function (row, col, num) {
+  Game.prototype.setColor = function(row, col, num) {
     // 给对应有颜色的方块添加类名
     $("tr")
       .eq(row)
@@ -46,16 +47,31 @@
       .addClass("c" + num);
   };
   // 清屏功能
-  Game.prototype.clear = function () {
+  Game.prototype.clear = function() {
     for (var i = 0; i < this.row; i++) {
       for (var j = 0; j < this.col; j++) {
         $("tr").eq(i).children("td").eq(j).removeClass();
       }
     }
   };
-  Game.prototype.start = function () {
+  // 事件监听
+  Game.prototype.bindEvent = function() {
+    // 备份
     var self = this;
-    this.timer = setInterval(function () {
+    $(document).keydown(function(event) {
+      if (event.keyCode == 37) {
+        // 判断是否有向左移动的能力
+        self.block.checkLeft();
+      } else if (event.keyCode == 39) {
+        // 判断是否有向右移动的能力
+        self.block.checkRight();
+      }
+      // console.log(event.keyCode);
+    });
+  }
+  Game.prototype.start = function() {
+    var self = this;
+    this.timer = setInterval(function() {
       // 清屏
       self.clear();
       // 渲染方块
